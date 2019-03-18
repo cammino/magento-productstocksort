e03beb0d3e4f3d011be54e22d02e176dac0c71b9
Rodrigo Ananias <r.ananias03@gmail.com>
2019-03-18 11:04:35 -0300

    #11248 - Refatora o modulo Productstocksort usando o phpcs

--- Model/Observer.php
+++ Model/Observer.php
@@ -1,14 +1,34 @@
 <?php
+/**
+ * Observer.php
+ *
+ * @category Cammino
+ * @package  Cammino_Productstocksort
+ * @author   Cammino Digital <suporte@cammino.com.br>
+ * @license  http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link     https://github.com/cammino/magento-productstocksort
+ */
+
 class Cammino_Productstocksort_Model_Observer
 {
+    /**
+     * Function responsible for check the quantity in stock,
+     * if zero, add product at the bottom of the page
+     *
+     * @param object $observer Magento observer
+     *
+     * @return null
+     */
     public function catalogProductCollectionLoadBefore(Varien_Event_Observer $observer)
     {        
-        //Pega a query do select, armazena em uma variável e
-        //verifica se uma parte do select "_inventory_table" contém na string
+        // It takes the select query, stores it in a variable and
+        // checks if a part of the select "_inventory_table" contains in the string
+
         $collection = $observer->getCollection();
         $sql = $collection->getSelect()->__toString();
 
         if (strpos($sql, "_inventory_table") === false) {
+
             $collection->getSelect()->joinLeft(
                 array(
                     "_inventory_table" => $collection->getTable('cataloginventory/stock_item')
@@ -19,11 +39,16 @@ class Cammino_Productstocksort_Model_Observer
             
             $collection->addExpressionAttributeToSelect(
                 'on_top',
-                "(CASE WHEN (((_inventory_table.use_config_manage_stock = 1) AND (_inventory_table.is_in_stock = 1)) OR  ((_inventory_table.use_config_manage_stock = 0) AND (1 - _inventory_table.manage_stock + _inventory_table.is_in_stock >= 1))) THEN 1 ELSE 0 END)",
+                "(CASE WHEN (((_inventory_table.use_config_manage_stock = 1) AND 
+                (_inventory_table.is_in_stock = 1)) OR 
+                ((_inventory_table.use_config_manage_stock = 0) AND 
+                (1 - _inventory_table.manage_stock + _inventory_table.is_in_stock >= 1))) THEN 1 ELSE 0 END)",
                 array()
             );
-            $collection->getSelect()->order('on_top DESC');
+
             // Make sure on_top is the first order directive
+            $collection->getSelect()->order('on_top DESC');
+
             $order = $collection->getSelect()->getPart('order');
             array_unshift($order, array_pop($order));
             $collection->getSelect()->setPart('order', $order);
