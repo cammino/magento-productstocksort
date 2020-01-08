5abc0b08d96235658461c58d5715fee81a4ff99c
Luan Dantas <luandr92@gmail.com>
2020-01-08 10:37:30 -0300

    #3830 - Fix sort by relevance

--- Model/Observer.php
+++ Model/Observer.php
@@ -24,34 +24,34 @@ class Cammino_Productstocksort_Model_Observer
         // It takes the select query, stores it in a variable and
         // checks if a part of the select "_inventory_table" contains in the string
 
-        $collection = $observer->getCollection();
-        $sql = $collection->getSelect()->__toString();
+        // $collection = $observer->getCollection();
+        // $sql = $collection->getSelect()->__toString();
 
-        if (strpos($sql, "_inventory_table") === false) {
+        // if (strpos($sql, "_inventory_table") === false) {
 
-            $collection->getSelect()->joinLeft(
-                array(
-                    "_inventory_table" => $collection->getTable('cataloginventory/stock_item')
-                ),
-                "_inventory_table.product_id = e.entity_id",
-                array('is_in_stock', 'manage_stock')
-            );
+        //     $collection->getSelect()->joinLeft(
+        //         array(
+        //             "_inventory_table" => $collection->getTable('cataloginventory/stock_item')
+        //         ),
+        //         "_inventory_table.product_id = e.entity_id",
+        //         array('is_in_stock', 'manage_stock')
+        //     );
             
-            $collection->addExpressionAttributeToSelect(
-                'on_top',
-                "(CASE WHEN (((_inventory_table.use_config_manage_stock = 1) AND 
-                (_inventory_table.is_in_stock = 1)) OR 
-                ((_inventory_table.use_config_manage_stock = 0) AND 
-                (1 - _inventory_table.manage_stock + _inventory_table.is_in_stock >= 1))) THEN 1 ELSE 0 END)",
-                array()
-            );
+        //     $collection->addExpressionAttributeToSelect(
+        //         'on_top',
+        //         "(CASE WHEN (((_inventory_table.use_config_manage_stock = 1) AND 
+        //         (_inventory_table.is_in_stock = 1)) OR 
+        //         ((_inventory_table.use_config_manage_stock = 0) AND 
+        //         (1 - _inventory_table.manage_stock + _inventory_table.is_in_stock >= 1))) THEN 1 ELSE 0 END)",
+        //         array()
+        //     );
 
-            // Make sure on_top is the first order directive
-            $collection->getSelect()->order('on_top DESC');
+        //     // Make sure on_top is the first order directive
+        //     $collection->getSelect()->order('on_top DESC');
 
-            $order = $collection->getSelect()->getPart('order');
-            array_unshift($order, array_pop($order));
-            $collection->getSelect()->setPart('order', $order);
-        }
+        //     $order = $collection->getSelect()->getPart('order');
+        //     array_unshift($order, array_pop($order));
+        //     $collection->getSelect()->setPart('order', $order);
+        // }
     }
 }
